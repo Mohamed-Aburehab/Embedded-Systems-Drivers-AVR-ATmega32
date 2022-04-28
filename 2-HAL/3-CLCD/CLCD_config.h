1ac1f27cb24bed9c447c92d057753a891773c5c0
Mohamed Aburehab <mohamed.aburehab@gmail.com>
2022-04-28 07:38:43 +0200

    CLCD update

--- 2-HAL/3-CLCD/CLCD_config.h
+++ 2-HAL/3-CLCD/CLCD_config.h
@@ -12,7 +12,12 @@
 // choose between 4 and 8
 #define CLCD_MODE                   4
 
-#define CLCD_DATA_PORT              DIO_u8PORTC
+#define CLCD_DATA_PORT              DIO_u8PORTA
+// for 4 bit mode choose the data pins:
+#define CLCD_DATA_PIN_7             DIO_u8PIN1
+#define CLCD_DATA_PIN_6             DIO_u8PIN2
+#define CLCD_DATA_PIN_5             DIO_u8PIN3
+#define CLCD_DATA_PIN_4             DIO_u8PIN4
 
 /* for 4 bit mode choose between: 
             1 for first  half of the port (Pins : 0, 1, 2, 3)
@@ -21,7 +26,7 @@
 
 #define CLCD_DATA_PORT_PART         2
 
-#define CLCD_CTRL_PORT              DIO_u8PORTB
+#define CLCD_CTRL_PORT              DIO_u8PORTC
 #define CLCD_RS_PIN                 DIO_u8PIN0
 #define CLCD_RW_PIN                 DIO_u8PIN1
 #define CLCD_E_PIN                  DIO_u8PIN2
--- 2-HAL/3-CLCD/CLCD_private.h
+++ 2-HAL/3-CLCD/CLCD_private.h
@@ -7,48 +7,20 @@
 /*****************************************************************************/
 /*****************************************************************************/
 
-
-void CLCD_private_voidSetDataPortForHigherOrder(u8 Copy_u8Command,u8 Copy_CLCD_DATA_PORT, u8 Copy_CLCD_DATA_PORT_PART){
-    switch (Copy_CLCD_DATA_PORT_PART)
-    {
-    case 1:
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN0, GET_BIT(Copy_u8Command,7));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN1, GET_BIT(Copy_u8Command,6));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN2, GET_BIT(Copy_u8Command,5));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN3, GET_BIT(Copy_u8Command,4));
-        break;
-    case 2:
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN7, GET_BIT(Copy_u8Command,7));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN6, GET_BIT(Copy_u8Command,6));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN5, GET_BIT(Copy_u8Command,5));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN4, GET_BIT(Copy_u8Command,4));
-        break;
-    
-    default:
-        break;
-    }
-}
-
-void CLCD_private_voidSetDataPortForLowerOrder(u8 Copy_u8Command, u8 Copy_CLCD_DATA_PORT, u8 Copy_CLCD_DATA_PORT_PART){
-    switch (Copy_CLCD_DATA_PORT_PART)
-    {
-    case 1:
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN0, GET_BIT(Copy_u8Command,3));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN1, GET_BIT(Copy_u8Command,2));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN2, GET_BIT(Copy_u8Command,1));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN3, GET_BIT(Copy_u8Command,0));
-        break;
-    case 2:
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN7, GET_BIT(Copy_u8Command,3));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN6, GET_BIT(Copy_u8Command,2));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN5, GET_BIT(Copy_u8Command,1));
-        DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, DIO_u8PIN4, GET_BIT(Copy_u8Command,0));
-        break;
-    
-    default:
-        break;
-    }
-}
+//void CLCD_private_voidSetDataPortForHigherOrder(u8 Copy_u8Command,u8 Copy_CLCD_DATA_PORT, u8 Copy_CLCD_DATA_PORT_PART){
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_7, GET_BIT(Copy_u8Command,7));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_6, GET_BIT(Copy_u8Command,6));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_5, GET_BIT(Copy_u8Command,5));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_4, GET_BIT(Copy_u8Command,4));
+//}
+//
+//void CLCD_private_voidSetDataPortForLowerOrder(u8 Copy_u8Command, u8 Copy_CLCD_DATA_PORT, u8 Copy_CLCD_DATA_PORT_PART){
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_7, GET_BIT(Copy_u8Command,3));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_6, GET_BIT(Copy_u8Command,2));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_5, GET_BIT(Copy_u8Command,1));
+//    DIO_u8SetPinValue(Copy_CLCD_DATA_PORT, CLCD_DATA_PIN_4, GET_BIT(Copy_u8Command,0));
+//
+//}
 
 u8 CLCD_private_u8GetnumberOfDigits(u32 Copy_u32Number){
     u8 local_u8Counter = 0;
